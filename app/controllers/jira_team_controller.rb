7ecd4a92702ac2a7363e4df0094d97ab31ce1a5b
Jessica Pamdeth <jessica.pamdeth@zendesk.com>
2024-09-26 11:49:25 -0400

    Try disabling Resolved select box if Resolved status isn't selected.

--- app/controllers/jira_team_controller.rb
+++ app/controllers/jira_team_controller.rb
@@ -4,7 +4,9 @@ class JiraTeamController < ApplicationController
       teams = Array(params[:project_key])
       statuses = Array(params[:status])
 
-      unless teams.empty? || statuses.empty?
+      if teams.empty? || statuses.empty?
+        @error = "Please select a team and a status"
+      else
         created = params[:created]
         updated = params[:updated]
         resolved = params[:resolved]
@@ -12,12 +14,12 @@ class JiraTeamController < ApplicationController
         jql_conditions = []
         jql_conditions << "project IN (#{teams.map { |team| "\"#{team}\"" }.join(', ')})" unless teams.empty?
         jql_conditions << "status IN (#{statuses.map { |status| "\"#{status}\"" }.join(', ')})" unless statuses.empty?
-        jql_conditions << "created >= -#{created}d" unless created == 'Any'
-        jql_conditions << "updated >= -#{updated}d" unless updated == 'Any'
-        jql_conditions << "resolved >= -#{resolved}d" unless resolved == 'Any'
+        jql_conditions << "created >= -#{created}d" unless created == "Any"
+        jql_conditions << "updated >= -#{updated}d" unless updated == "Any"
+        jql_conditions << "resolved >= -#{resolved}d" unless resolved == "Any" if statuses.include?("Resolved")
 
         unless jql_conditions.empty?
-          jql = jql_conditions.join(' AND ') + " AND issuetype IN (Task, Story) order by created DESC"
+          jql = jql_conditions.join(" AND ") + " AND issuetype NOT IN (Epic) order by created DESC"
           start_at = 0
           max_results = 50
           all_issues = []
@@ -25,14 +27,15 @@ class JiraTeamController < ApplicationController
           loop do
             issues = JIRA_CLIENT.Issue.jql(jql, fields: %w[summary assignee issuetype priority status resolution customfield_10004 customfield_19880 customfield_19971], start_at: start_at, max_results: max_results)
             all_issues.concat(issues)
-            break if issues.size < max_results
+            break if issues.size < max_results || all_issues.size >= 500
             start_at += max_results
           end
 
           @issues = all_issues
+          if all_issues.size >= 500
+            @error = "A maximum of 500 issues can be returned. Please refine your search."
+          end
         end
-      else
-        @error = 'Please select a team and a status'
       end
     rescue => e
       @error = e.message
